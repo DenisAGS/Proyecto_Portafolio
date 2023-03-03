c864ec31514ad547dc5414b67281bb9575be5a3f
DenisAGS <denissearelyg@gmail.com>
2023-03-03 14:15:36 -0600

    Initial commit

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web, desarrollo aplicaciones moviles y administracion en bases de datos.')
+  .typeString('Desarrollo sitios web, desarrollo aplicaciones móviles y administración en bases de datos.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
